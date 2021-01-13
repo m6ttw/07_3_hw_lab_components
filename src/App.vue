7877e0272cb56247325a01ddf004373b6375db08
m6ttw <matthewrhyswilliams@live.com>
2021-01-13 16:36:38 +0000

    MPV done I think (no CSS)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
-<template lang="html">
-  <p>{{ message }}</p>
+<template>
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+      <country-detail :country='selectedCountry'></country-detail>
+    </div>
+  </div>
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
+import { eventBus } from './main.js';
+
 export default {
   name: "app",
   data(){
     return {
-      message: "Placeholder Text"
+      countries: [],
+      selectedCountry: null
     }
+  },
+  mounted(){
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country
+    })
+  },
+  components: {
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div v-if='country'>
+    <h3>{{country.name}}</h3>
+    <p>Capital: {{country.capital}}</p>
+    <p>Subregion: {{country.subregion}}</p>
+    <p>Population: {{country.population}}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+
+</script>
+
+<style>
+
+</style>
